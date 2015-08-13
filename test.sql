dade29e8f99849ccb9bb20cdefb53529bd2d3ca3
Myeongsun Jung <piee@daumcorp.com>
2015-08-13 10:44:10 +0900

    test
    test

---
+++ test.sql
@@ -0,0 +1,13 @@
+﻿select count(*) from test;
+
+select * from test limit 10;
+
+update test set col3 = '1';
+
+select pg_relation_size('test');
+
+
+
+select pg_size_pretty(pg_relation_size('test'));;
+
+show autovacuum;
